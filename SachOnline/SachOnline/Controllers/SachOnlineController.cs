85bb1f8dbba079355d61f8771cd78e4c7b35ea2a
doanxuandong <doanxuandong8303@gmail.com>
2023-10-16 15:05:48 +0700

    lab4

---
+++ SachOnline/SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,71 @@
+﻿using SachOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        dbSachOnlineDataContext data = new dbSachOnlineDataContext();
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd;
+            return PartialView(listNhaXuatBan);
+        }
+
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSachBanNhieu = LaySachMoi(6);
+            return PartialView(listSachBanNhieu);
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SachTheoChuDe(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+
+        public ActionResult SachTheoNhaXuatBan(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = from s in data.SACHes where s.MaSach == id select s;
+            return View(sach.Single());
+        }
+
+    }
+}
